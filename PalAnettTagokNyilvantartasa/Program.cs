7759d3de19c219ded3bf0983b5ead379bdff1093
xnetta92 <palanett92@gmail.com>
2023-12-10 13:18:18 +0100

    Add project files.

---
+++ PalAnettTagokNyilvantartasa/Program.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalAnettTagokNyilvantartasa
+{
+    internal class Program
+    {
+        static List<Tag> tagList = new List<Tag>();
+        static MySqlConnection connection = null;
+        static MySqlCommand command = null;
+        static void Main(string[] args)
+        {
+            beolvasas();
+            tagokListazasa();
+            //ujTagFelvetele();
+            //tagTorlese();
+
+            Console.WriteLine("\nProgram vége");
+            Console.ReadLine ();
+        }
+
+        private static void tagokListazasa()
+        {
+            foreach (Tag item in tagList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void beolvasas()
+        {
+            MySqlConnectionStringBuilder sb = new MySqlConnectionStringBuilder();
+            sb.Clear();
+            sb.Server = "localhost";
+            sb.UserID = "anett";
+            sb.Password = "7112";
+            sb.Database = "tagdij";
+            sb.CharacterSet = "utf8";
+            connection = new MySqlConnection(sb.ConnectionString);
+            command = connection.CreateCommand();
+            try
+            {
+                connection.Open();
+                command.CommandText = "SELECT * FROM `ugyfel`";
+                using (MySqlDataReader dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        Tag uj = new Tag(dr.GetInt32("azon"), dr.GetString("nev"), dr.GetInt32("szulev"), dr.GetInt32("iszam"), dr.GetString("orsz"));
+                        tagList.Add(uj);
+                    }
+                }
+                connection.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ PalAnettTagokNyilvantartasa/Tag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalAnettTagokNyilvantartasa
+{
+    internal class Tag
+    {
+        public int azon;
+        public string nev;
+        public int szulev;
+        public int iszam;
+        public string orsz;
+
+        public Tag(int azon, string nev, int szulev, int iszam, string orsz)
+        {
+            this.azon = azon;
+            this.nev = nev;
+            this.szulev = szulev;
+            this.iszam = iszam;
+            this.orsz = orsz;
+        }
+        public override string ToString()
+        {
+            return nev;
+        }
+    }
+}
