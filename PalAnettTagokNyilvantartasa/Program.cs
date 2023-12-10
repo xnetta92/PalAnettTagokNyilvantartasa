f4b17a6d46ac8aab58e204ce7641c2d7884c2566
xnetta92 <palanett92@gmail.com>
2023-12-10 14:16:22 +0100

    kész

--- PalAnettTagokNyilvantartasa/Program.cs
+++ PalAnettTagokNyilvantartasa/Program.cs
@@ -18,10 +18,58 @@ namespace PalAnettTagokNyilvantartasa
             beolvasas();
             tagokListazasa();
             //ujTagFelvetele();
-            //tagTorlese();
-
-            Console.WriteLine("\nProgram vége");
+            //tagTorlese(1014);
+            Console.WriteLine("\nProgram vége!");
             Console.ReadLine ();
+        }
+
+        private static void tagTorlese(int azon)
+        {
+            
+            command.CommandText = $"DELETE FROM `ugyfel` WHERE azon = '{azon}'";
+            command.Parameters.Clear();
+            try
+            {
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Hiba a deletben!");
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+        }
+
+        private static void ujTagFelvetele()
+        {
+            Tag tag = new Tag(1014, "Pálma Fa", 1995, 1094, "UK");
+            command.CommandText = "INSERT INTO `ugyfel`(`azon`, `nev`, `szulev`, `irszam`, `orsz`) VALUES (@azon, @nev, @szulev, @irszam, @orsz)";
+            command.Parameters.Clear();
+            command.Parameters.AddWithValue("@azon", tag.azon);
+            command.Parameters.AddWithValue("@nev", tag.nev);
+            command.Parameters.AddWithValue("@szulev", tag.szulev);
+            command.Parameters.AddWithValue("@irszam", tag.irszam);
+            command.Parameters.AddWithValue("@orsz", tag.orsz);
+            try
+            {
+                if (connection.State != System.Data.ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+                command.ExecuteNonQuery ();
+                connection.Close ();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Hiba az insertben!");
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
         }
 
         private static void tagokListazasa()
@@ -59,6 +107,7 @@ namespace PalAnettTagokNyilvantartasa
             }
             catch (MySqlException ex)
             {
+                Console.WriteLine("Hiba a Selectben!");
                 Console.WriteLine(ex.Message);
                 Environment.Exit(0);
             }
